0632d1960bad1cf253886210a20de6ca7f127634
Justin Makaila <justinmakaila@gmail.com>
2017-11-29 15:12:52 -0500

    bumped version to RAS 3.0

--- Tests/MoyaProviderIntegrationTests.swift
+++ Tests/MoyaProviderIntegrationTests.swift
@@ -255,7 +255,7 @@ class MoyaProviderIntegrationTests: QuickSpec {
                         expect(log).to( contain("Request Headers: [:]") )
                         expect(log).to( contain("HTTP Request Method: GET") )
                         expect(log).to( contain("Response:") )
-                        expect(log).to( contain("{ URL: https://api.github.com/zen } { status code: 200, headers") )
+                        expect(log).to( contain("{ URL: https://api.github.com/zen } { Status Code: 200, Headers") )
                         expect(log).to( contain("\"Content-Length\" = 43;") )
                     }
                 }
